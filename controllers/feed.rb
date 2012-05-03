541b7bc913de9873c6966a27db5fc0a47aeca18e
Sho Hashimoto <hashimoto@shokai.org>
2012-05-04 02:08:36 +0900

    enable on-memory cache for feed

--- controllers/feed.rb
+++ controllers/feed.rb
@@ -3,12 +3,14 @@
 get '/feed/akizuki.rss' do
   content_type 'application/xml'
   @title = "秋月電子通商 新着商品"
+  items = TmpCache.get('akizuki') || TmpCache.set('akizuki', Akizuki.get_items(20), 600)
+
   rss = RSS::Maker.make('2.0') do |rss|
     rss.channel.about = "#{app_root}#{env['PATH_INFO']}"
     rss.channel.title = @title
     rss.channel.link = Akizuki.list_url
     rss.channel.description = @title
-    Akizuki.get_items(20).each do |a|
+    items.each do |a|
       i = rss.items.new_item
       i.title = a[:title]
       i.link = a[:url]
