709fd2b90d58f2a3481d9303bc4f26b8eb14e85e
Sho Hashimoto <hashimoto@shokai.org>
2012-05-04 01:46:30 +0900

    fix rss link

--- controllers/feed.rb
+++ controllers/feed.rb
@@ -4,7 +4,7 @@ get '/feed/akizuki.rss' do
   content_type 'application/xml'
   @title = "秋月電子通商 新着商品"
   rss = RSS::Maker.make('2.0') do |rss|
-    rss.channel.about = "#{app_root}/feeds/akizuki.rss"
+    rss.channel.about = "#{app_root}#{env['PATH_INFO']}"
     rss.channel.title = @title
     rss.channel.link = Akizuki.list_url
     rss.channel.description = @title
